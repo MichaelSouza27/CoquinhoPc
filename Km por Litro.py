6a0ae45c216ad4527e5b6d19c9da029a1ee3a248
MichaelSouza27 <michaelsouza2708@gmail.com>
2025-09-04 20:44:22 -0300

    Add files via upload

---
+++ Km por Litro.py
@@ -0,0 +1,6 @@
+C = int(input("Digite a distância que Leonardo vai correr: "))
+N = int(input("Digite o comprimento da pista: "))
+
+#Ponto de término
+ponto_termino = C % N
+print("Ponto de término:", ponto_termino)
