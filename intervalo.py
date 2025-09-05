966f6af9c5a297e6af8f3d76af5d4254678b85cf
MichaelSouza27 <michaelsouza2708@gmail.com>
2025-09-04 21:19:08 -0300

    Add files via upload

---
+++ intervalo.py
@@ -0,0 +1,8 @@
+i = int(input("quantos intervalos você fez?"))
+print("Os km tem que seguir no mesmo número")
+k = int(input("quantos km você fez a cada intervalo?"))
+
+# Final da Viagem
+total = k * i
+print("Foram percorridos")
+print(total)
